b5f5c1b31f43af0d420e1903661c7e10d1f2e9ca
Marcus O'Yang <18moyang@gmail.com>
2022-07-19 23:52:10 +1000

    frontend structure setup, api routes yet to be done

---
+++ next-auth/pages/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "../components/layout"
+
+export default function IndexPage() {
+  return (
+    <Layout>
+      <h1>COMP6452 Project 2</h1>
+      <p>
+        Site run on Next.js with TypeScript and NextAuth.
+      </p>
+    </Layout>
+  )
+}
