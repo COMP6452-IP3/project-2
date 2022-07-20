3afeb0208e0a0b4f3d975e88a008d59887dde7ff
Spenser Tan <51882646+davo1233@users.noreply.github.com>
2022-07-20 12:21:38 +1000

    edited file namese

--- database/upload_web3.js
+++ database/upload_web3.js
@@ -1,3 +1,6 @@
+// Created by Spenser Tan
+// 19/07/2022
+// Some code from https://web3.storage/docs/
 import process from 'process'
 import minimist from 'minimist'
 import { Web3Storage, getFilesFromPath } from 'web3.storage'
@@ -18,14 +21,14 @@ async function main () {
 
   const storage = makeStorageClient()
   const files = []
-
+  files = getFiles()
   for (const path of args._) {
     const pathFiles = await getFilesFromPath(path)
     files.push(...pathFiles)
   }
 
   console.log(`Uploading ${files.length} files`)
-  const cid = await storage.put(files)
+  const cid = storeFiles(files,storage);
   console.log('Content added with CID:', cid)
 }
   
@@ -33,7 +36,29 @@ function getAccessToken() {
   return process.env.WEB3STORAGE_TOKEN;
 }
 
-function makeStorageClient () {
+function makeStorageClient() {
   return new Web3Storage({ token: getAccessToken() })
+}
+
+function getFiles() {
+  const fileInput = document.querySelector('input[type="file"]')
+  return fileInput.files
+}
+
+function makeFileObjects() {
+  const obj = { hello: 'world' }
+  const blob = new Blob([JSON.stringify(obj)], { type: 'application/json' })
+
+  const files = [
+    new File(['contents-of-file-1'], 'plain-utf8.txt'),
+    new File([blob], 'hello.json')
+  ]
+  return files
+}
+
+async function storeFiles(files,storage) {
+  const cid = await storage.put(files)
+  console.log('stored files with cid:', cid)
+  return cid
 }
 main()
