ab39c5f27f46231660dec18b10c15ee8bfaa8fa7
Spenser Tan <51882646+davo1233@users.noreply.github.com>
2022-07-25 03:29:52 +1000

    updated permissions to fit with the figma

--- contracts/_permissions.sol
+++ contracts/_permissions.sol
@@ -6,61 +6,69 @@ pragma solidity >=0.7.0 <0.9.0;
  * @title Permissions
  * @dev Implements fair use permissions for individuals to use the smart contract
  */
-contract Permissions {
 
+ 
+contract Licensing {
 
-    struct OwnershipTransactions {
-        address NFT; //address of the NFT in the ownership transaction
-        address licensee; //address of the licensee
-        bytes32 transactionID;
+    struct Artwork {
+        string title;
+        uint year;
+        Permissions[] licenses;
+    }
+
+    struct Permissions {
+        address licensee;
+        uint royalty;
+        bool paid;
+    }
+
+    // A list of registered artworks in the system
+    mapping (uint => Artwork) public artworks;  // CID -> Artwork
+    mapping (uint => bytes32) public transactions; //CID -> transactionID
+    address artist;
+    function addArtwork (uint cid, string memory _title, uint _year) public {
+        artworks[cid] = Artwork(_title, _year);
     }
 
 
-    address public artist;
-
-    mapping (uint => address) public NFTToLicensee;
-    mapping (address => uint) LicenseeNFTCount;
-
-
-    OwnershipTransactions[] transactions;
-    
-
-
-    /** 
-     * @dev assign address of the smart contract to the artist
-     */
-    constructor() {
-        artist = msg.sender;
-
-    }
-
-    /** 
-     * @dev Give permissions for the licensee to use the artwork. May only be called by the 'artist'.
-     * @param _licensee address of licensee
-     * @param _NFT address of the NFT artwork
-     */
-    function givePermissionsToLicensee(address _licensee, address _NFT) public returns (bytes32) {
-        require(
-            msg.sender == artist,
-            "Only artist can give permission."
-        );
-        
-        OwnershipTransactions memory transaction = OwnershipTransactions(_NFT,_licensee,'');
-        bytes32 transactionID = generateTransactionID(transaction);
-        transaction.transactionID = transactionID;
-        transactions.push(transaction);
-        uint id = transactions.length - 1;
-        NFTToLicensee[id] = _licensee;
-        LicenseeNFTCount[_licensee]++;
-        
+    // called by artist
+    function grantPermission (uint _cid, address _licensee, uint _royalty, bool _paid) restricted public returns (bytes32) {
+        Artwork storage aw = artworks[_cid];
+        Permissions memory _transaction = Permissions(_licensee,_royalty,_paid);
+        uint currLic = getLicensesCount(_cid);
+        aw.licenses.push(Permissions(_licensee,_royalty,_paid));       
+        bytes32 transactionID = generateTransactionID(_transaction);
+        transactions[_cid] = transactionID;
         return transactionID;
     }
+
     /** 
      * @dev create a hash for the transaction id.
      * @param _transaction transaction object
      */
-    function generateTransactionID(OwnershipTransactions memory _transaction) private pure returns (bytes32) {
-        return keccak256(abi.encode(_transaction.NFT, _transaction.licensee));
+    function generateTransactionID(Permissions memory _transaction) public pure returns (bytes32) {
+        return keccak256(abi.encode(_transaction.licensee, _transaction.royalty));
+    }   
+
+    // called by licensee
+    function getArtwork (uint cid) public returns (bool) { 
+        artworks[cid].licenses;
+    // loop thru and find licence which matches msg.sender
+    // check if theyve paid (if not, call pay function & set paid to true)
+    // return true (if everything success)
+    return false;
     }
+
+// TWO ROYALTY FUNCTIONS yes
+
+    modifier restricted() {
+        require(msg.sender == artist, "Can only be executed by the artist.");
+        _;
+    }
+
+
+    
+
+
 
 }
