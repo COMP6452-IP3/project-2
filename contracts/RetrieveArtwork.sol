9293c9d2bb89e9ee507b99d74b2142acf9b55867
z5257051 <byung.c.min@student.unsw.edu.au>
2022-07-25 22:32:12 +1000

    added method of paying, withdrawing royalties

--- contracts/RetrieveArtwork.sol
+++ contracts/RetrieveArtwork.sol
@@ -15,54 +15,87 @@ pragma solidity ^0.8.0;
  * @title RetrieveArtwork.sol
  * @dev Handles access and retrieval of artwork from Web3 storage. Includes verification
  */
-contract RetrieveArtwork {
+contract Licensing {
+    struct Artwork {
+        address artist;
+        string title;
+        uint year;
+        Permission[] licenses;
+    }
 
-    address payable public artistWallet;
-    string private cID;            // todo: may access this cID in a different way
-    bool public royaltyPaid = false;  // has the royalty been paid yet?
+    struct Permission {
+        address licensee;
+        uint royalty;
+        bool paid;
+    }
+
+    address payable public artist;      // payable allows artist to receive ether
+
+    // A list of registered artworks in the system
+    mapping (uint => Artwork) public artworks; // CID -> Artwork
 
     constructor() {
-        artistWallet = msg.sender;   // artist calls this function
+        artist = payable(msg.sender);   // ensures artist is a payable account
     }
 
-    /** Flow of this smart contract
-     *  Licensee wants access to the artwork
-     *  - Call this SC with the pID of the permission transaction they have sent in
-     *  - todo: function will search through the pIDs on the BC and retrieve matching ones 
-     *    - if not found, function fails; no access   (might need for searching: https://docs.etherscan.io/api-endpoints/accounts)
-     *  - check that the licencee address matches sender
-     *    - if not, function fails; no access
-     *  - if matches:
-     *    - if royalty has not yet been paid, call for payment to be made
-     *    - find the cID of the artwork in web3 and return it to the website
-     */
+    function addArtwork (uint cid, string memory _title, uint _year) public restricted {
+        artworks[cid].artist = msg.sender;
+        artworks[cid].title = _title;
+        artworks[cid].year = _year;
+    }
+
+    function getPermissionsList(uint cid) view public returns (Permission[] memory) {
+        return artworks[cid].licenses;
+    }  
     
-    function requestAccess(string memory pID) public authenticateLicensee returns (string memory) {
-        // todo: do the search
-        if (!royaltyPaid) {
-            payRoyaltyFee;
+
+    // called by artist
+    function grantPermission (uint cid, address user, uint _royalty) public restricted {
+        artworks[cid].licenses.push(Permission(user, _royalty, false));
+    }
+
+    
+    // called by licensee
+    function getArtwork (uint cid) payable public returns (bool) { 
+        uint len = artworks[cid].licenses.length;
+        for (uint i = 0; i < len; i++) {
+            Permission memory _permission = artworks[cid].licenses[i];
+            if (msg.sender == _permission.licensee) {
+                // the user is someone with permission
+                // checks if the royalty is paid or not
+                if (_permission.paid == false) {
+                    // royalty payments need to be done
+                    // the line below calls the fallback function
+                    // todo: do we need to verify 
+                    (bool success, ) = address(this).call{value: _permission.royalty}("");
+                    require(success, "Failed to send Ether");
+                    _permission.paid = true;
+                }
+                // If paid, all good to receive the artwork
+                // todo: make call to the off-chain system to return the artwork
+                return true;
+            }
         }
-        return cID;     // todo: how to grab cID
+        // If we're out of this loop without matching with the licensee then they cannot retrieve artwork
+        return false;
     }
 
 
-    function payRoyaltyFee() public payable {
-        artistWallet.transfer(msg.value);
+    // called by artist to withdraw all ETH stored on this contract
+    function withdrawRoyalty() restricted public {
+        // get total royalties stored on this contract
+        uint amount = address(this).balance;
+
+        // send all Ether to owner
+        (bool success, ) = artist.call{value: amount}("");
+        require(success, "Failed to withdraw Ether");
     }
 
-    function withdrawRoyaltyFee() authenticateArtist public {
-        
-    }
+    fallback () payable external {}      // overwrite fallback function to accept ETH
 
-    /// @notice sender must be the licensee on permission ID 
-    modifier authenticateLicensee() {
-        require(msg.sender == pID.licensee, "Does not match licensee on permissions."); // todo: how to grab pID data
-        _;
-    }
-
-    /// @notice sender must be the original artist
-    modifier authenticateArtist() {
-        require(msg.sender == artist, "Does not match licensee on permissions."); // todo: how to grab pID data
+    /// @notice must be called by the artist
+    modifier restricted() {
+        require(msg.sender == artist, "Can only be executed by the artist.");
         _;
     }
 }
