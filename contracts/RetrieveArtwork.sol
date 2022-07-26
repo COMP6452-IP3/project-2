2fbb96a1bab0aaf05af47fd2bf28a83441c516ac
z5257051 <byung.c.min@student.unsw.edu.au>
2022-07-27 03:55:09 +1000

    added test suite (in 'tests' folder)

--- contracts/RetrieveArtwork.sol
+++ contracts/RetrieveArtwork.sol
@@ -2,14 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-// todo: (ask) could be better to have this smart contract 
-// as part of the same smart contract as issuing permissions
-// since (A) will have access to the "owner" of the smart
-// contract (artist) to handle payments, (B) will be easier
-// to check if royalty has been paid (one time payment can
-// be tracked with bool). (C) easier to check if the licensee
-// which requests to use the artwork has indeed been given
-// permission
 
 /**
  * @title RetrieveArtwork.sol
@@ -17,7 +9,6 @@ pragma solidity ^0.8.0;
  */
 contract Licensing {
     struct Artwork {
-        address artist;
         string title;
         uint year;
         Permission[] licenses;
@@ -42,7 +33,6 @@ contract Licensing {
     }
 
     function addArtwork (uint cid, string memory _title, uint _year) public restricted {
-        artworks[cid].artist = msg.sender;
         artworks[cid].title = _title;
         artworks[cid].year = _year;
     }
@@ -50,16 +40,14 @@ contract Licensing {
     function getPermissionsList(uint cid) view public returns (Permission[] memory) {
         return artworks[cid].licenses;
     }  
-    
 
     // called by artist
     function grantPermission (uint cid, address user, uint _royalty) public restricted {
         artworks[cid].licenses.push(Permission(user, _royalty, false));
     }
-
     
     // called by licensee
-    function getArtwork (uint cid) payable public returns (bool) { 
+    function getArtwork (uint cid) payable public returns (bool) {
         uint len = artworks[cid].licenses.length;
         for (uint i = 0; i < len; i++) {
             Permission storage _permission = artworks[cid].licenses[i];
@@ -68,7 +56,6 @@ contract Licensing {
                 // checks if the royalty is paid or not
                 if (_permission.paid == false) {
                     // royalty payments need to be done
-                    // todo: do we need to verify 
                     require(msg.value == _permission.royalty, "Incorrect royalty amount.");
                     // the line below calls the fallback function
                     (bool success, ) = address(this).call{value: msg.value}("");
---
+++ tests/RetrieveArtwork_test.sol
@@ -0,0 +1,161 @@
+// SPDX-License-Identifier: UNLICENSED
+        
+pragma solidity >=0.8.00 <0.9.0;
+import "remix_tests.sol";           // This import is automatically injected by Remix
+import "remix_accounts.sol";
+import "../contracts/RetrieveArtwork.sol";
+import "https://github.com/GNSPS/solidity-bytes-utils/blob/master/contracts/BytesLib.sol";
+
+
+contract retrieveArtworkTestSuite is Licensing {
+
+    using BytesLib for bytes;
+
+    // Accounts
+    address acc0;
+    address acc1;
+    address acc2;
+
+    // Set up
+    function beforeAll() public {
+        acc0 = TestsAccounts.getAccount(0);   
+        acc1 = TestsAccounts.getAccount(1);
+        acc2 = TestsAccounts.getAccount(2);
+    }
+
+    /// Original sender is the artist
+    function origArtistTest() public {
+       Assert.equal(artist, acc0, "Artist should be acc0");
+    }
+
+    /// -------------------- ARTWORKS TEST --------------------
+    /// Artist can add artworks - should increase artworks in system
+    function addArtworksTest() public {
+       addArtwork(1, "Artwork 1", 2001);
+       Assert.equal(artworks[1].title, "Artwork 1", "Title 1 not set properly.");
+       Assert.equal(artworks[1].year, 2001, "Year 1 not set properly.");
+       addArtwork(2, "Artwork 2", 2002);
+       Assert.equal(artworks[2].title, "Artwork 2", "Title 2 not set properly.");
+       Assert.equal(artworks[2].year, 2002, "Year 2 not set properly.");
+    }
+
+
+    /// Only artist can add artworks
+    /// #sender: account-1
+    function addArtworksFailTest() public {
+        (bool success, bytes memory result) = address(this).delegatecall(abi.encodeWithSignature("addArtwork(uint256,string,uint256)", 3, "Artwork 3", 2003));
+        Assert.ok(!success, "WRONG");
+        if (success == false) {
+            string memory reason = abi.decode(result.slice(4, result.length - 4), (string));
+            Assert.equal(reason, "Can only be executed by the artist.", "Failed with unexpected reason");
+        } else {
+            Assert.ok(false, "Method Execution Should Fail");
+        }
+    }
+
+
+    /// -------------------- PERMISSIONS TEST --------------------
+    /// Grant permissions to particular users
+    function addPermissionsTest() public {
+       grantPermission(1, acc1, 1000);
+       grantPermission(1, acc2, 2000);
+       grantPermission(2, acc1, 1000);
+       Assert.equal(artworks[1].licenses.length, 2, "Two permissions should be registered.");
+
+       Permission memory _permission1 = artworks[1].licenses[0];
+       Assert.equal(_permission1.licensee, acc1, "Account not set properly.");
+       Assert.equal(_permission1.royalty, 1000, "Royalty not set properly.");
+       Assert.equal(_permission1.paid, false, "Paid state not set properly.");
+    }
+
+    /// Only artists can grant permissions
+    /// #sender: account-1
+    function addPermissionsFailTest() public {
+        (bool success, bytes memory result) = address(this).delegatecall(abi.encodeWithSignature("grantPermission(uint256,address,uint256)", 2, acc1, 10));
+        if (success == false) {
+            string memory reason = abi.decode(result.slice(4, result.length - 4), (string));
+            Assert.equal(reason, "Can only be executed by the artist.", "Failed with unexpected reason");
+        } else {
+            Assert.ok(false, "Method Execution Should Fail");
+        }
+    }
+
+    /// -------------------- GET ARTWORKS TEST --------------------
+    /// A licensee with a permission to specified artist can get permission when paying correct royalty
+    /// #sender: account-1
+    /// #value: 1000
+    function retrieveArtworksTest() payable public {
+        (bool success, bytes memory result)  = address(this).delegatecall(abi.encodeWithSignature("getArtwork(uint256)", 1));
+        Assert.ok(success, "Should not throw any errors.");
+        bool successGotArtwork = abi.decode(result, (bool));
+        Assert.ok(successGotArtwork, "Should have retrieved artwork.");
+        Assert.ok(artworks[1].licenses[0].paid, "Paid state not updated properly.");
+    }
+
+    /// A licensee should be able to retrieve the artwork without sending royalty after theyve sent it once
+    /// #sender: account-1
+    /// #value: 0
+    function retrieveArtworksAfterPaymentTest() payable public {
+        (bool success, bytes memory result)  = address(this).delegatecall(abi.encodeWithSignature("getArtwork(uint256)", 1));
+        Assert.ok(success, "Should not throw any errors.");
+        bool successGotArtwork = abi.decode(result, (bool));
+        Assert.ok(successGotArtwork, "Should succeed in getting artwork after once paid royalty.");
+    }
+
+    /// Not accepted with incorrect royalty amount
+    /// #sender: account-2
+    /// #value: 1000
+    function retrieveArtworksWrongRoyaltyTest() public {
+        (bool success, bytes memory result) = address(this).delegatecall(abi.encodeWithSignature("getArtwork(uint256)", 1));
+        if (success == false) {
+            string memory reason = abi.decode(result.slice(4, result.length - 4), (string));
+            Assert.equal(reason, "Incorrect royalty amount.", "Failed with unexpected reason");
+        } else {
+            Assert.ok(false, "Method Execution Should Fail");
+        }
+    }
+
+    /// Not accepted if caller has not been given permission
+    /// #sender: account-2
+    /// #value: 1000
+    function retrieveArtworksNotPermittedTest() public {
+        (bool success, bytes memory result) = address(this).delegatecall(abi.encodeWithSignature("getArtwork(uint256)", 2));
+        Assert.ok(success, "Should not throw any errors.");
+        bool successGotArtwork = abi.decode(result, (bool));
+        Assert.ok(!successGotArtwork, "Should fail in getting artwork.");
+    }
+
+    /// -------------------- WITHDRAW ROYALTY TEST --------------------
+    /// Only artist can withdraw the balance of the SC
+    /// #sender: account-1
+    function withdrawAsNotArtistFailTest() public {
+        (bool success, bytes memory result) = address(this).delegatecall(abi.encodeWithSignature("withdrawRoyalty()"));
+        if (success == false) {
+            string memory reason = abi.decode(result.slice(4, result.length - 4), (string));
+            Assert.equal(reason, "Can only be executed by the artist.", "Failed with unexpected reason");
+        } else {
+            Assert.ok(false, "Method Execution Should Fail");
+        }
+    }
+
+    /// The artist can receive the royalty payments
+    function withdrawAsArtistTest() public {
+        uint initBalance = address(this).balance;
+        Assert.equal(initBalance, 1000, "Royalties not paid properly.");
+        withdrawRoyalty();
+
+        uint newBalance = address(this).balance;
+        Assert.equal(newBalance, 0, "Royalties not withdrawn properly.");
+    }
+
+
+    /// The artist cannot receive the royalty payments again
+    function withdrawAgainShouldBeZeroTest() public {
+        uint initBalance = address(this).balance;
+        withdrawRoyalty();
+
+        uint newBalance = address(this).balance;
+        Assert.equal(initBalance - newBalance, 0, "Royalties were withdrawn twice.");
+    }
+
+}
